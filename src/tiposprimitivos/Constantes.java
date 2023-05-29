ebb2bbaf6708ba4842e1ee4ef8bbf32378d8ba13
Anne Ribeiro <anne.adsribeiro@gmail.com>
2023-05-29 18:10:16 -0300

    first commit

---
+++ src/tiposprimitivos/Constantes.java
@@ -0,0 +1,10 @@
+package tiposprimitivos;
+
+public class Constantes {
+    
+public static void main(String[] args) {
+    final double ACELERAÇÃO_DA_GRAVIDADE = 10.0;
+    
+    System.out.println("Valor da Constante: " + ACELERAÇÃO_DA_GRAVIDADE);
+}
+}
